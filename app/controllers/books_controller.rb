a78be94c6b19f7fafdb395aee712ceedfed8456e
Ahlonko Kowouvi <ahlonko.kowouvi@gmail.com>
2022-06-16 15:52:03 +0200

    user model

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,24 @@
 class BooksController < ApplicationController
+
+  def index
+    @books = Books.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to restaurant_path(@book)
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :language, :synopsis)
+  end
+
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :user
   belongs_to :author
+
+  validates :title, :language, :synopsis, presence: true
 end
